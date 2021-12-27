c115bac5aba34841940c62c39574f177b8d1cf35
Andrew Walker <awalker@ixsystems.com>
2021-12-27 07:48:43 -0500

    Revert dbus fixes in prep for merging 4.0
    Finalized fix for NULL deref in  dbus thread initializion
    took different form.

--- src/include/export_mgr.h
+++ src/include/export_mgr.h
@@ -282,10 +282,8 @@ static inline void tmp_get_exp_paths(struct tmp_export_paths *tmp,
 
 	if (gr != NULL)
 		tmp->tmp_pseudopath = gsh_refstr_get(gr);
-	else if (exp->cfg_pseudopath != NULL)
-		tmp->tmp_pseudopath = gsh_refstr_dup(exp->cfg_pseudopath);
 	else
-		tmp->tmp_pseudopath = gsh_refstr_dup("No Export");
+		tmp->tmp_pseudopath = gsh_refstr_dup(exp->cfg_pseudopath);
 
 	rcu_read_unlock();
 }
